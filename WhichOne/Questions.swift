026353c896b29dd4c742c4a8c62cb901fc90d9b1
Julian <julian@raschke.de>
2024-12-30 12:46:04 +0100

    Update Questions.swift

--- WhichOne/Questions.swift
+++ WhichOne/Questions.swift
@@ -507,7 +507,7 @@ let hanziDefinitions: [Character: HanziDefinition] = {
     definitions["科"] = HanziDefinition(pinyin: "tbd",    translation: "tbd")
     definitions["料"] = HanziDefinition(pinyin: "tbd",    translation: "tbd")
 
-    // more: 遨邀 繁擊 摧攜 船般 殼穀...
+    // more: 遨邀 繁擊 摧攜 船般 殼穀 消減 消滅 消費 寻导 捨拾 繼斷 徵徽...
 
     return definitions
 }()
